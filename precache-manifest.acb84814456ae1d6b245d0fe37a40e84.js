ef8452975cfe34bd3b092a044ad3c13342393e87
Kris Knez <kristijankej@gmail.com>
2020-07-11 23:06:40 +0200

    First website design

---
+++ precache-manifest.acb84814456ae1d6b245d0fe37a40e84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "040908cf26e8bcc07fdaf8573be2b44b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e72ef3c34dcc8761e9c1",
+    "url": "/static/css/main.ba14df76.chunk.css"
+  },
+  {
+    "revision": "6f8a4402de0e3809c199",
+    "url": "/static/js/2.f837608c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f837608c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e72ef3c34dcc8761e9c1",
+    "url": "/static/js/main.6068aab5.chunk.js"
+  },
+  {
+    "revision": "451b3b2fa319339d7ab3",
+    "url": "/static/js/runtime-main.14517863.js"
+  }
+]);
